c30fa8087d82f35737b6113584aacb76a6844882
Doug <doug.steinberg@gmail.com>
2017-06-02 13:22:59 -0400

    Added favourite link page

--- server.rb
+++ server.rb
@@ -8,7 +8,14 @@ get '/portfolio' do
   erb :gallery
 end
 
-
 get '/about' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+
+get '/favourites' do
+  @fav_links = ['<a href="https://thewalrus.ca/">The Walrus</a>', '<a href="https://github.com/dls3">GitHub</a>', '<a href="http://www.espn.com/">ESPN</a>', '<a href="https://www.netflix.com/browse">Netflix</a>', '<a href="https://theringer.com/">The Ringer</a>']
+  erb :fav
 end
